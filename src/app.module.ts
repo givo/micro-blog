d5cdf272c9d2f7faa91fba96b4a192d79e97a682
givo <matangivoni@gmail.com>
2018-10-20 00:14:53 +0300

    using posts module inside app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, PostsController],
+  imports: [MongooseModule.forRoot('mongodb://localhost/test'), PostsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
