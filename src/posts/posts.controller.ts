56aa9c6428aded2b6f53d8203cf34a57d029dbaa
givo <matangivoni@gmail.com>
2018-10-20 00:06:06 +0300

    implement create() and findOne() inside posts controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('posts')
-export class PostsController { }
+export class PostsController {
+    constructor(private readonly postsService: PostsService){
+
+    }
+
+    @Post()
+    public create(@Body() dto: CreatePostDto){
+        return this.postsService.create(dto);
+    }
+
+    @Get('/:postId')
+    public findOne(@Param('postId') postId){
+        return this.postsService.findOne(postId);
+    }
+}
